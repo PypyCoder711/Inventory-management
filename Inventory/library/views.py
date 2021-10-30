9c08f87167a223467d6055ac5e2ddab8af157d1f
hkatkade <hkatkade7@gmail.com>
2021-10-31 00:08:32 +0530

    Login/register pages added

--- Inventory/library/views.py
+++ Inventory/library/views.py
@@ -1,8 +1,9 @@
+from django.http.response import HttpResponse
 from django.shortcuts import render
 from django.http import HttpRequest
 
 def home(request):
-    return HttpRequest('<h1>Blog home</h1>')
+    return render(request,'library/base.html')
 
 
 
